7d78240a7388f3fdb5877dab456149f73fa64559
Ayush Sharma <ayusharma1706@gmail.com>
2026-06-09 10:03:46 +0530

    [Ayush Sharma] Add: Programming Elements Assignment Completed

---
+++ core-java-practice/gcr-codebase/programming-elements/programming-elements-practice/lab2-practice/HeightConversion.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class HeightConversion {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        double heightInCm;
+
+        System.out.print("Enter height in cm: ");
+        heightInCm = input.nextDouble();
+
+        double totalInches = heightInCm / 2.54;
+        int feet = (int) (totalInches / 12);
+        double inches = totalInches % 12;
+
+        System.out.println(
+                "Your Height in cm is " + heightInCm +
+                " while in feet is " + feet +
+                " and inches is " + inches
+        );
+
+        input.close();
+    }
+}
---
+++ core-java-practice/gcr-codebase/programming-elements/programming-elements-practice/lab2-practice/KilometerToMiles.java
@@ -0,0 +1,11 @@
+class KilometerToMiles {
+
+    public static void main(String[] args) {
+
+        double distanceInKm = 10.8;
+        double miles = distanceInKm / 1.6;
+
+        System.out.println("The distance " +
+                distanceInKm + " km in miles is " + miles);
+    }
+}
